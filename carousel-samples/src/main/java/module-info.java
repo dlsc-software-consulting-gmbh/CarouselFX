02edcebec8e7d698d358da6a711c559cca350605
leewyatt <leewyatt7788@gmail.com>
2026-03-26 00:06:04 +0900

    Add module-info.java for core library and samples to enable Java module system

---
+++ carousel-samples/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.dlsc.carousel.samples {
+    requires com.dlsc.carousel;
+    requires javafx.controls;
+    requires javafx.swing;
+    requires com.dlsc.gemsfx;
+    requires java.desktop;
+
+    exports com.dlsc.carousel.samples;
+}
---
+++ carousel/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.dlsc.carousel {
+    requires javafx.controls;
+
+    exports com.dlsc.carousel;
+    exports com.dlsc.carousel.animation;
+    exports com.dlsc.carousel.navigator;
+}
